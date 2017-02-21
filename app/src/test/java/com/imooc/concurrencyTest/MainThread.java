a24ced3c8f5ef05db1d7eabe5522058ae90bbd93
WeaponZhi <584098488@qq.com>
2017-02-21 12:49:47 +0800

    主线程代码

--- app/src/test/java/com/imooc/concurrencyTest/MainThread.java
+++ app/src/test/java/com/imooc/concurrencyTest/MainThread.java
@@ -3,9 +3,13 @@ package com.imooc.concurrencyTest;
 import org.junit.Test;
 
 /**
- * Created by xiaozhizhi on 2017/02/20.
+ * MainThread 主线程
+ * <p>
+ * author: 张冠之 <br>
+ * time:   2017/02/21 12:48 <br>
+ * e-mail: 584098488@qq.com <br>
+ * </p>
  */
-
 public class MainThread {
     @Test
     public void main(){
